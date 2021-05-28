898c56dd6dbd40f46acf080358f49927def69f51
Ajay5201 <ajay123mohan@gmail.com>
2021-05-28 21:19:45 +0530

    make it better1

--- main.py
+++ main.py
@@ -12,7 +12,8 @@ import databases, sqlalchemy
 
 
 import os
-Db_URL="postgresql://tvmrhbjnytvuro:61a94c7df671bc80c67bfa75b48e76ad1c4b26634bac79a2dd0f74b51adde18f@ec2-107-22-245-82.compute-1.amazonaws.com:5432/do60c522tmtg3"
+#Db_URL="postgresql://tvmrhbjnytvuro:61a94c7df671bc80c67bfa75b48e76ad1c4b26634bac79a2dd0f74b51adde18f@ec2-107-22-245-82.compute-1.amazonaws.com:5432/do60c522tmtg3"
+Db_URL="postgresql://postgres:swethA@127.0.0.1:5432/postgres"
 database=databases.Database(Db_URL)
 metadata=sqlalchemy.MetaData()
 
